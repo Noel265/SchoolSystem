2750a030db37a4a3ff6a68537a58e095ae8c69eb
Noel Denja <denjanoelroy66@gmail.com>
2026-06-04 13:02:29 +0200

    update

--- backend/SchoolSystem.API/Program.cs
+++ backend/SchoolSystem.API/Program.cs
@@ -48,7 +48,11 @@ builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:5174",
+                "http://127.0.0.1:5174")
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
